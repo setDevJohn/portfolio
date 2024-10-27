c151117f7f1b20ca57c592f6076fbde3f36e45a7
Jhony Freitas <jhony00._@hotmail.com>
2024-10-27 01:27:38 -0300

    docs: Update config

--- eslint.config.js
+++ eslint.config.js
@@ -24,28 +24,10 @@ export default tseslint.config(
       "block-spacing": ["error", "always"],
       "space-before-function-paren": ["error", "always"],
       "object-curly-spacing": ["error", "always"],
-      "no-console": ["warn"],
-      curly: ["error", "all"],
-      "consistent-return": "error",
-      "no-alert": "error",
       eqeqeq: ["error", "always"],
       "react/prop-types": "off",
-      "react/jsx-boolean-value": ["error", "always"],
-      "react/jsx-sort-props": "warn",
-      "react/jsx-no-duplicate-props": "error",
-      "@typescript-eslint/explicit-function-return-type": [
-        "warn",
-        {
-          allowExpressions: true,
-          allowTypedFunctionExpressions: true,
-        },
-      ],
       "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/no-unnecessary-type-assertion": "error",
-      "@typescript-eslint/type-annotation-spacing": "error",
       "prefer-const": "error",
-      "no-use-before-define": "error",
-      "no-extra-parens": ["error", "all"],
     },
   }
 );
