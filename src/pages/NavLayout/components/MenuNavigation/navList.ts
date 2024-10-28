3d2033531310cb6402bdf18439375c0ac2428e28
Jhony Freitas <jhony00._@hotmail.com>
2024-10-28 20:42:40 -0300

    feat: Adding routes

--- src/pages/NavLayout/components/MenuNavigation/navList.ts
+++ src/pages/NavLayout/components/MenuNavigation/navList.ts
@@ -1,5 +1,7 @@
 export const navList = [
   { label: 'Serviços', path: '/services' },
+  { label: 'Portfólio', path: '/portfolio' },
+  { label: 'Habilidades', path: '/habilities' },
   { label: 'Sobre', path: '/about' },
   { label: 'Contato', path: '/contact' },
 ];
