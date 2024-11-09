d20832872f0ffe684a0fde245008aeec5cb47604
Jhony Freitas <jhony00._@hotmail.com>
2024-11-09 12:15:03 -0300

    feat: Create service page

--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -1,6 +1,53 @@
+import frontIcon from '@assets/icons/front.png';
+import backIcon from '@assets/icons/back.png';
+import infraIcon from '@assets/icons/infra.png';
+import { CardIcon, CardsContainer, CardServices, CardText, CardTextContainer, DescContainer, PageContainer, ServiceText, ServiceTitle, Title } from './styles';
+import { useState } from 'react';
+
 export const Services = () => {
+  const [selectedCard, setSelectedCard] = useState('Frontend');
+
+  const cardList = [
+    {
+      icon: frontIcon,
+      title: 'Frontend',
+      text: 'Criação de páginas funcionais e dinâmicas na web'
+    },
+    {
+      icon: backIcon,
+      title: 'Backend',
+      text: 'Criação de aplicações e rotas para gerenciamento de banco de dados.'
+    },
+    {
+      icon: infraIcon,
+      title: 'Infra',
+      text: 'Criação de infraestrutura, cofiguração e hospedagem da aplicação.'
+    },
+  ];
+
   return (
-    <>
-    </>  
+    <PageContainer>
+      <CardsContainer>
+        {cardList.map(({ icon, title, text }) => (
+          <CardServices 
+            key={title} 
+            $selected={title === selectedCard}
+            onClick={() => setSelectedCard(title)}
+          >
+            <CardIcon src={icon}/>
+
+            <CardTextContainer>
+              <Title>{title}</Title>
+              <CardText>{text}</CardText>
+            </CardTextContainer>
+          </CardServices>
+        ))}
+
+        <DescContainer key={selectedCard}>
+          <ServiceTitle>{selectedCard}</ServiceTitle>
+          <ServiceText>Lorem ipsum dolor sit amet, consectetur adipisci elit,, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.</ServiceText>
+        </DescContainer>
+      </CardsContainer>
+    </PageContainer>  
   );
 };
---
+++ src/pages/Services/styles.ts
@@ -0,0 +1,80 @@
+import styled, { keyframes } from 'styled-components';
+
+type CardServicesProps = {
+  $selected: boolean;
+}
+
+const slideIn = keyframes`
+  0% {
+    transform: translateY(90%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+`;
+
+export const PageContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  flex: 1;
+  padding: 15px 0;
+`;
+export const CardsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 30px;
+  margin-top: 9%;
+  width: 100%;
+`;
+export const CardServices = styled.div<CardServicesProps>`
+  display: flex;
+  align-items: center;
+  gap: 25px;
+  background-color: ${({ theme }) => theme.primaryColor};
+  border-radius: 10px;
+  padding: 16px;
+  width: 100%;
+  transition: all 0.6s ease;
+  box-shadow: ${({ $selected, theme }) => (
+    `0px 2px 11px -3px ${$selected ? theme.contrastColor : '#0008'}`
+  )};
+  min-width: 200px;
+  max-width: 285px;
+  min-height: 125px;
+  cursor: pointer;
+`;
+export const CardTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+export const Title = styled.h2`
+  color: ${({ theme }) => theme.contrastColor};
+  font-size: 1.8rem;
+  font-weight: 600;
+`;
+export const CardText = styled.p`
+  font-size: 1.4rem;
+`;
+export const CardIcon = styled.img``;
+export const DescContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  margin-top: 35px;
+  width: 100%;
+  max-width: 960px;
+  animation: ${slideIn} 0.8s ease-out forwards;
+`;
+export const ServiceTitle = styled(Title)`
+  font-size: 2.2rem;
+`;
+export const ServiceText = styled.p`
+  font-size: 1.8rem;
+  font-weight: 500;
+`;
