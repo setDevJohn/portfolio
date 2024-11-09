c6bbbdc73e1ec9801dfb54c31c979912f5e3c03d
Jhony Freitas <jhony00._@hotmail.com>
2024-11-09 12:19:04 -0300

    chore: export pages

---
+++ src/pages/index.ts
@@ -0,0 +1,8 @@
+import { NavLayout } from './NavLayout/NavLayout';
+import { Services } from './Services/Services';
+import { Portfolio } from './Portfolio/Portfolio';
+import { Habilities } from './Habilities/Habilities';
+import { About } from './About/About';
+import { Contact } from './Contact/Contact';
+
+export { NavLayout, Services, Portfolio, Habilities, About, Contact };
