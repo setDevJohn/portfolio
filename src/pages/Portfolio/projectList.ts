ee55e9be68b673e8dc711b831026f5dfeb275b71
Jhony Freitas <jhony00._@hotmail.com>
2025-01-17 23:47:39 -0300

    feat: Add project list

--- src/pages/Portfolio/projectList.ts
+++ src/pages/Portfolio/projectList.ts
@@ -1,9 +1,14 @@
-import project1 from '@assets/images/image.png';
+import ipAddressTracker from '@assets/images/ip-address-tracker.png';
+import interactiveForm from '@assets/images/interactive-form.png';
+import githubProfile from '@assets/images/github-profile.png';
+import lavandinha from '@assets/images/lavandinha.png';
+import pokedex from '@assets/images/pokedex.png';
+import codeCafe from '@assets/images/code-cafe.png';
  
 export const projectList = [
   {
-    path: project1,
-    name: 'Projeto 1',
+    path: ipAddressTracker,
+    name: 'Rastreador de endereço de IP',
     repoLink: 'https://github.com/',
     deployLink: 'https://github.com/',
     stacks: [
@@ -13,8 +18,8 @@ export const projectList = [
     ],
   },
   {
-    path: project1,
-    name: 'Projeto 2',
+    path: interactiveForm,
+    name: 'Formulário interativo',
     repoLink: 'https://github.com/',
     deployLink: 'https://github.com/',
     stacks: [
@@ -24,8 +29,8 @@ export const projectList = [
     ],
   },
   {
-    path: project1,
-    name: 'Projeto 3',
+    path: githubProfile,
+    name: 'Requisição de perfil do GitHub',
     repoLink: 'https://github.com/',
     deployLink: 'https://github.com/',
     stacks: [
@@ -35,8 +40,8 @@ export const projectList = [
     ],
   },
   {
-    path: project1,
-    name: 'Projeto 4',
+    path: lavandinha,
+    name: 'Lavandinha bordados',
     repoLink: 'https://github.com/',
     deployLink: 'https://github.com/',
     stacks: [
@@ -46,8 +51,8 @@ export const projectList = [
     ],
   },
   {
-    path: project1,
-    name: 'Projeto 5',
+    path: pokedex,
+    name: 'Pokedex',
     repoLink: 'https://github.com/',
     deployLink: 'https://github.com/',
     stacks: [
@@ -57,8 +62,8 @@ export const projectList = [
     ],
   },
   {
-    path: project1,
-    name: 'Projeto 6',
+    path: codeCafe,
+    name: 'Code Café',
     repoLink: 'https://github.com/',
     deployLink: 'https://github.com/',
     stacks: [
