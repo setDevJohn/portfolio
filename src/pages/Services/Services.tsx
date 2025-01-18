e8409b37eb8b4e006a4df22ec49ebdb7c665d72a
Jhony Freitas <jhony00._@hotmail.com>
2025-01-18 01:14:50 -0300

    feat: Update cards services

--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react';
-import frontIcon from '@assets/icons/front.png';
-import backIcon from '@assets/icons/back.png';
-import infraIcon from '@assets/icons/infra.png';
 import { PageContainer } from '@styles/pageStyle';
+import { cardList } from './cardList';
 import { 
   CardIcon, 
   CardsContainer, 
@@ -18,23 +16,6 @@ import {
 export const Services = () => {
   const [selectedCard, setSelectedCard] = useState('Frontend');
 
-  const cardList = [
-    {
-      icon: frontIcon,
-      title: 'Frontend',
-      text: 'Criação de páginas funcionais e dinâmicas na web'
-    },
-    {
-      icon: backIcon,
-      title: 'Backend',
-      text: 'Criação de aplicações e rotas para gerenciamento de banco de dados.'
-    },
-    {
-      icon: infraIcon,
-      title: 'Infra',
-      text: 'Criação de infraestrutura, cofiguração e hospedagem da aplicação.'
-    },
-  ];
 
   return (
     <PageContainer>
@@ -56,7 +37,10 @@ export const Services = () => {
 
         <DescContainer key={selectedCard}>
           <ServiceTitle>{selectedCard}</ServiceTitle>
-          <ServiceText>Lorem ipsum dolor sit amet, consectetur adipisci elit,, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.</ServiceText>
+          
+          <ServiceText>
+            {cardList.find(({ title }) => title === selectedCard).desc}
+          </ServiceText>
         </DescContainer>
       </CardsContainer>
     </PageContainer>  
---
+++ src/pages/Services/cardList.ts
@@ -0,0 +1,24 @@
+import frontIcon from '@assets/icons/front.png';
+import backIcon from '@assets/icons/back.png';
+import infraIcon from '@assets/icons/infra.png';
+
+export const cardList = [
+  {
+    icon: frontIcon,
+    title: 'Frontend',
+    text: 'Desenvolvimento de interfaces web interativas e responsivas para uma experiência do usuário de alta qualidade.',
+    desc: 'Ofereço serviços especializados em desenvolvimento frontend, garantindo que suas páginas web sejam modernas, funcionais e esteticamente agradáveis. Utilizo as melhores práticas de design responsivo para assegurar que sua aplicação seja acessível em diversos dispositivos e navegadores. Trabalho com tecnologias como HTML, CSS, JavaScript, e frameworks como React, Angular ou Vue.js, para criar interfaces dinâmicas e intuitivas. Meu foco é traduzir ideias e requisitos em experiências visuais impactantes e eficientes.'
+  },
+  {
+    icon: backIcon,
+    title: 'Backend',
+    text: 'Desenvolvimento de soluções robustas para gerenciamento de dados e lógica de negócios.',
+    desc: 'Especialista em desenvolvimento backend, projeto APIs e sistemas seguros para o gerenciamento de dados e lógica de negócios. Tenho experiência com Node.js, e Express, além de trabalhar com bancos de dados relacionais, como MySQL e PostgreSQL. Meu objetivo é criar estruturas escaláveis e otimizadas para atender às demandas da sua aplicação, garantindo alta performance e segurança. Cada solução é planejada para integrar-se perfeitamente com o frontend e a infraestrutura.'
+  },
+  {
+    icon: infraIcon,
+    title: 'Infra',
+    text: 'Configuração inicial e hospedagem de aplicações web de forma prática e eficiente.',
+    desc: 'Ofereço serviços de configuração inicial e hospedagem para aplicações web, garantindo que sua aplicação esteja pronta para ser acessada pelo público de maneira rápida e segura. Tenho experiência em configurar servidores, gerenciar domínios, e integrar serviços de hospedagem como Digital Ocean ou plataformas tradicionais. Meu foco está em disponibilizar sua aplicação de forma funcional, realizando ajustes necessários para assegurar estabilidade e um desempenho consistente.'
+  },
+];
