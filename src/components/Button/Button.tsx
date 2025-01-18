663b04700c1375766d2bf2232151048c5f4d06ec
Jhony Freitas <jhony00._@hotmail.com>
2025-01-17 21:05:23 -0300

    feat: Add style button per property

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,17 @@
+import { CSSObject } from 'styled-components';
 import { ButtonContainer, ButtonIcon } from './styles';
 
 type ButtonProps = {
   text: string
   icon: string
   type?: string
+  style: CSSObject
   handleClick?: () => void
 }
 
-export const Button = ({ text, icon, type, handleClick }: ButtonProps) => {
+export const Button = ({ text, icon, type, style, handleClick }: ButtonProps) => {
   return ( 
-    <ButtonContainer type={type || 'button'}>
+    <ButtonContainer style={style} type={type || 'button'}>
       {text}
       <ButtonIcon 
         src={icon} 
