811beeff2921cee1ba33ef0b60b59bd83664cde1
Jhony Freitas <jhony00._@hotmail.com>
2025-01-18 02:52:22 -0300

    feat: Add toastfy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { AppRoutes } from './routes';
 import { GlobalStyle } from '@styles/globalStyles';
 import { ThemeColorProvider } from '@context/ThemeContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export function App () {
   return (
     <ThemeColorProvider>
       <GlobalStyle />
       <AppRoutes />
+      <ToastContainer />
     </ThemeColorProvider>
   );
 }
