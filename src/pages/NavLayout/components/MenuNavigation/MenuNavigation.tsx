875abb3d78989ba0bd74c057a1536696393d2d73
Jhony Freitas <jhony00._@hotmail.com>
2025-01-20 22:57:16 -0300

    feat: Add  resposive menu

--- src/pages/NavLayout/components/MenuNavigation/MenuNavigation.tsx
+++ src/pages/NavLayout/components/MenuNavigation/MenuNavigation.tsx
@@ -1,19 +1,42 @@
+import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { navList } from './navList';
-import { Item, Nav, NavigationContainer, NavigationList } from './styles';
+import menuIcon from '@assets/icons/menu.svg';
+import closeIcon from '@assets/icons/close.svg';
+import { Icon, Item, Nav, NavigationContainer, NavigationList } from './styles';
 
 export const MenuNavigation = () => {
+  const [active, setActive] = useState(false);
+
   const navigate = useNavigate();
   const location = useLocation();
+
+  function handleNavigate (path: string) {
+    setActive(false);
+    navigate(path);
+  }
   
   return ( 
     <NavigationContainer>
-      <Nav>
+      <Icon src={menuIcon} alt="Ícone de menu" onClick={() => setActive(true)}/>
+
+      <Nav $open={active}>
+        <Icon 
+          src={closeIcon}
+          alt="Ícone de fechar"
+          onClick={() => setActive(false)}
+          style={{ 
+            width: '40px',
+            marginTop: '14px',
+            alignSelf: 'end'
+          }}
+        />
+
         <NavigationList>
           {navList.map(({ label, path }, i) =>(
             <Item 
               key={i} 
-              onClick={() => navigate(path)}
+              onClick={() => handleNavigate(path)}
               $active={path === location.pathname} 
             >
               {label}
--- src/pages/NavLayout/components/MenuNavigation/styles.ts
+++ src/pages/NavLayout/components/MenuNavigation/styles.ts
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
+import { sizes } from '@styles/mediaSizes';
 
 type ItemProps = {
   $active: boolean;
+}
+type NavProps = {
+  $open: boolean;
 }
 
 export const NavigationContainer = styled.header`
@@ -9,24 +13,65 @@ export const NavigationContainer = styled.header`
   justify-content: center;
   margin-bottom: 15px;
   width: 100%;
+
+  @media (${sizes.mobile}) {
+    justify-content: start;
+  }
 `;
 
-export const Nav = styled.nav``;
+export const Icon = styled.img`
+  display: none;
+  width: 30px;
+  cursor: pointer;
+
+  @media (${sizes.mobile}) {
+    display: block;
+  }
+`;
+
+export const Nav = styled.nav<NavProps>`
+  @media (${sizes.mobile}) {
+    position: absolute;
+    z-index: 1;
+    inset: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background: ${({ theme }) => theme.secondaryColor};
+    transition: ease all 0.4s;
+    width: ${({ $open }) => $open ? '30%' : '0%'};
+    min-width: ${({ $open }) => $open ? '130px' : '0px'};
+    overflow: hidden;
+  }
+`;
 
 export const NavigationList = styled.ul`
   display: flex;
   gap: 30px;
+
+  @media (${sizes.mobile}) {
+    flex-direction: column;
+    margin: 18px 15px;
+    gap: 25px;
+  }
 `;
 
 export const Item = styled.li<ItemProps>`
-  /* font-family: "Public Sans", serif; */
   font-size: 1.9rem;
   font-weight: ${({ $active }) => $active ? 500 : 400};
   color: ${({ $active, theme }) => $active && theme.contrastColor};
   cursor: pointer;
+  padding: 2px 4px;
   transition: 0.4s ease-in-out;
 
   &:hover {
     transform: translateY(-2px);
+  }
+
+  @media (${sizes.mobile}) {
+    font-size: 2rem;
+    background-color: ${({ $active, theme }) => $active && theme.contrastColor};
+    color: ${({ $active }) => $active && '#000'};
+    border-radius: 10px;
   }
 `;
